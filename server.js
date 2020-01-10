e4c0731e51e1a39ef42a41336958dbeaee29348b
JanMichaelSe <janm.montalvo@gmail.com>
2020-01-10 01:44:04 -0800

    deployment stage

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.post("/register", register.handleRegister(db, bcrypt));
 
 app.put("/image", image.handleImage(db));
 
-app.listen(3000, () => {
-  console.log(`app is running on port 3000`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
